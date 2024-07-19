4f8e03c9e90e39168ea33314b9f105e963243b5b
fabrizio.bruno <fabrizio.bruno@we-plus.eu>
2024-07-19 13:02:46 +0200

    Hendler error pipe for ORU message from TD

--- src/main/java/com/mirth/prometeo/HL7Palm/Message/ACKResponse.java
+++ src/main/java/com/mirth/prometeo/HL7Palm/Message/ACKResponse.java
@@ -5,9 +5,11 @@ import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v25.message.ACK;
 import ca.uhn.hl7v2.model.v25.message.OML_O21;
+import ca.uhn.hl7v2.model.v25.segment.ERR;
 import ca.uhn.hl7v2.model.v25.segment.MSA;
 import ca.uhn.hl7v2.model.v25.segment.MSH;
 import com.mirth.prometeo.HL7Palm.SegmentFactory.SegmentFactoryACKResponseORLO22;
+import com.mirth.prometeo.HL7Palm.SegmentFactory.SegmentFactoryACKResponseORU;
 
 public class ACKResponse {
 
@@ -20,6 +22,21 @@ public class ACKResponse {
 
         MSA msaSegmentIntegrate = ack.getMSA();
         SegmentFactoryACKResponseORLO22.createMSASegmentIntegrateACKResponse(msaSegmentIntegrate, genericMessage);
+
+        return ack;
+    }
+
+    public ACK generateACKResponseORU(String hl7Message) throws HL7Exception {
+
+        MSH mshSegmentIntegrate = ack.getMSH();
+        SegmentFactoryACKResponseORU.createMSHSegmentIntegrateACKResponse(mshSegmentIntegrate, hl7Message);
+
+        MSA msaSegmentIntegrate = ack.getMSA();
+        SegmentFactoryACKResponseORU.createMSASegmentIntegrateACKResponse(msaSegmentIntegrate, mshSegmentIntegrate);
+
+        ERR errSegmentIntegrate = ack.getERR();
+        SegmentFactoryACKResponseORU.createERRSSegmentIntegrateACKResponse(errSegmentIntegrate);
+
 
         return ack;
     }
